41e60c379ae526082a19057f6e8dd8df90030093
PRAVIN RAJ <u2102721@siswa.um.edu.my>
2026-01-12 05:43:27 +0800

    link update

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://your-app-name.vercel.app"
+        "https://billa-rho.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
